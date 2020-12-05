ef5bac0b6db544821efa8f30e8c0d3db5579d1f8
Hari2019041 <hari19041@iiitd.ac.in>
2020-12-05 15:01:21 +0530

    Basic methods of ball added

--- main.py
+++ main.py
@@ -27,7 +27,43 @@ def setIcon(ICON):
 
 class Ball:
     def __init__(self):
-        pass
+        self.x = int(WIDTH/2)
+        self.y = int(HEIGHT/2)
+        self.x_speed = 1
+        self.y_speed = 1
+        self.size = 8
+
+    def start(self):
+        self.x_speed = 1
+        self.y_speed = 1
+
+    def show(self):
+        pygame.draw.circle(screen, WHITE, (self.x, self.y), self.size)
+
+    def move(self):
+        self.x += self.x_speed
+        self.y += self.y_speed
+
+    def hitEdges(self):
+        if self.y > HEIGHT or self.y < 0:
+            self.y_speed *= -1
+
+    def hitPaddle(self, paddle):
+        leftright = self.x > paddle.x or self.x < paddle.x - paddle.width
+        topbottom = self.y > paddle.y and self.y < paddle.y + paddle.length
+        if topbottom and leftright:
+            self.x_speed *= -1
+
+    def reset(self):
+        self.x_speed = 0
+        self.y_speed = 0
+        self.x = int(WIDTH/2)
+        self.y = int(HEIGHT/2)
+        self.start()
+
+    def leaveScreen(self):
+        if self.x > WIDTH or self.x < 0:
+            self.reset()
 
 
 class Player:
@@ -45,13 +81,19 @@ def main():
     setTitle(TITLE)
     setIcon(ICON)
     running = True
-
+    ball = Ball()
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
         screen.fill(BLACK)
+        ball.show()
+        # ball.start()
+        ball.move()
+        ball.hitEdges()
+        ball.leaveScreen()
+        pygame.display.update()
 
 
 main()
